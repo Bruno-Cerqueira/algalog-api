3d731f7094d7cf7a503001fd59196de31790c534
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-09-17 23:43:04 -0300

    Add Client Repository with basics functions. Add getMapping by id on client controller. Adjust Client Controller with autowired repository.

--- src/main/java/com/algaworks/algalog/algalogapi/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/algalogapi/api/controller/ClientController.java
@@ -1,22 +1,42 @@
 package com.algaworks.algalog.algalogapi.api.controller;
 
 import com.algaworks.algalog.algalogapi.domain.model.Client;
+import com.algaworks.algalog.algalogapi.domain.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClientRepository clientRepository;
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> index() {
-        return manager.createQuery("from Client", Client.class)
-                .getResultList();
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> find(@PathVariable Long clientId) {
+        return clientRepository.findById(clientId)
+                .map(client -> ResponseEntity.ok(client))
+                .orElse(ResponseEntity.notFound().build());
+
+        // Optional<Client> client = clientRepository.findById(clientId);
+
+        //if(client.isPresent()) {
+        //    return ResponseEntity.ok(client.get());
+        //}
+        //return ResponseEntity.notFound().build();
     }
 }
