289c49b96ba921624ebafe1d2f05c2b64a0d49f3
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-09-17 23:22:30 -0300

    Add entity into Client Controller to get data from database. Add database configurations to Client class entity.

--- src/main/java/com/algaworks/algalog/algalogapi/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/algalogapi/api/controller/ClientController.java
@@ -4,26 +4,19 @@ import com.algaworks.algalog.algalogapi.domain.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clients")
     public List<Client> index() {
-        Client client1 = new Client();
-        client1.setId(1L);
-        client1.setName("João");
-        client1.setEmail("joao@gmail.com");
-        client1.setPhone("11-202301232");
-
-        Client client2 = new Client();
-        client2.setId(2L);
-        client2.setName("Maria");
-        client2.setEmail("maria@gmail.com");
-        client2.setPhone("11-2213113");
-
-        return Arrays.asList(client2, client1);
+        return manager.createQuery("from Client", Client.class)
+                .getResultList();
     }
 }
--- src/main/java/com/algaworks/algalog/algalogapi/domain/model/Client.java
+++ src/main/java/com/algaworks/algalog/algalogapi/domain/model/Client.java
@@ -1,13 +1,27 @@
 package com.algaworks.algalog.algalogapi.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Client {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String phone;
+
+
 }
